e480a543bb0e28d8f844685466dfb7ec71791c81
Thomas Heinrichs <tom.heinri@gmail.com>
2022-06-05 17:51:37 -0400

    set up pixi

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
+import { useEffect } from 'react';
+import * as PIXI from 'pixi.js';
+import favicon from '../img/DDRJake_2.png';
 
 export default () => {
+    useEffect(() => {
+        let app = new PIXI.Application({ width: 640, height: 360 });
+
+        document.getElementById('app').appendChild(app.view);
+
+        let sprite = PIXI.Sprite.from(favicon);
+
+        app.stage.addChild(sprite);
+
+        sprite.scale.x = .25;
+        sprite.scale.y = .25;
+
+        return () => {
+            console.log('Component will be unmount')
+        }
+    }, []);
     return (
-        <div className="homePage">
-            <div>
-            </div>
+        <div id="app">
         </div>
     );
 };
